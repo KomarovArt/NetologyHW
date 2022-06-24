fc945de2a9bd1dcd4aafe7a786797d6839ec4e4b
KomarovAE <79115202517@ya.ru>
2022-06-24 13:24:40 +0300

    completing 3 of his tasks

--- taskThree.php
+++ taskThree.php
@@ -1,1 +1,27 @@
 <?php
+
+//$variable = 3.14;
+// $variable = 3;
+// $variable = 'one';
+// $variable = true;
+// $variable = null;
+ $variable = [];
+
+$type="";
+
+if (is_float($variable)){
+    $type="float";
+}elseif (is_int($variable)){
+    $type="int";
+}elseif (is_string($variable)){
+    $type = "string";
+}elseif (is_bool($variable)){
+    $type="bool";
+}elseif (is_null($variable)){
+    $type="null";
+}else{
+    $type = "other";
+}
+
+echo ("type is $type");
+
