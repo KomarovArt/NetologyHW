0639e072196f5797cc8d6972e1505af2577b8651
КомÐKomarovArtem <dethloony@yandex.ru>
2022-07-08 00:43:09 +0300

    Исправление путаницы с ветками, восстановление task_2

---
+++ task_2.php
@@ -0,0 +1,15 @@
+<?php
+
+fscanf(STDIN, "%d\n", $numberOne);
+
+fscanf(STDIN, "%d\n", $numberTwo);
+
+if ($numberTwo === 0) {
+    $stderr = fopen('php://stderr', 'w');
+    fwrite($stderr, "Делить на 0 нельзя");
+} elseif (is_numeric($numberOne) && is_numeric($numberTwo)) {
+    echo $numberOne / $numberTwo;
+} else {
+    $stderr = fopen('php://stderr', 'w');
+    fwrite($stderr, "Введите, пожалуйста, число");
+}
