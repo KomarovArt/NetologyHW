aa052b0cbf2fd5f0446d688a20b47ac228d537e3
KomarovAE <dethloony@yandex.ru>
2022-07-13 16:40:22 +0300

    Исправление недочетов с гитом и работой

--- task_3.php
+++ task_3.php
@@ -2,7 +2,8 @@
 $fullName ='';
 $fio = '';
 $surnameAndInitials = '';
-
+$pattern='/^\S/';
+$array=[];
 
 $stderr = fopen('php://stderr', 'w');
 fwrite($stderr,"Введите, пожалуйста, Имя".PHP_EOL);
@@ -15,6 +16,18 @@ fwrite($stderr,"Введите, пожалуйста,Отчество".PHP_EOL);
 fscanf(STDIN, "%s\n", $patronymicUser);
 
 
+$temporaryVariable = preg_match_all($pattern, $nameUser ,$outNameUser);
+$temporaryVariable = preg_match_all($pattern, $lastnameUser ,$outLastNameUser);
+$temporaryVariable = preg_match_all($pattern, $patronymicUser ,$outPatronymicUser);
 
-$stderr = fopen('php://stderr', 'w');
-fwrite($stderr,"$nameUser $lastnameUser $patronymicUser");
+//вывод фио
+$fio = strtoupper($outNameUser[0][0]).strtoupper($outLastNameUser[0][0]).strtoupper($outPatronymicUser[0][0]);
+echo $fio.PHP_EOL;
+
+//вывод Фамилия Имя Отчество
+$fullName = preg_replace($pattern, strtoupper($outNameUser[0][0]), $nameUser).' '.preg_replace($pattern, strtoupper($outLastNameUser[0][0]), $lastnameUser).' '.preg_replace($pattern, strtoupper($outPatronymicUser[0][0]), $patronymicUser);
+echo $fullName.PHP_EOL;
+
+//вывод Фамилия И.О.
+$surnameAndInitials = preg_replace($pattern, strtoupper($outNameUser[0][0]), $nameUser).' '.strtoupper($outLastNameUser[0][0]).'.'.strtoupper($outPatronymicUser[0][0]);
+echo $surnameAndInitials.PHP_EOL;
