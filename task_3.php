a9605cf5d607ab2b45f14fdbea2fbd7a9fbe5f88
KomarovAE <dethloony@yandex.ru>
2022-07-19 16:16:01 +0300

    Поиск недочета с кодировкой и кириллицей

--- task_3.php
+++ task_3.php
@@ -1,9 +1,10 @@
 <?php
-$fullName ='';
-$fio = '';
-$surnameAndInitials = '';
+ini_set('default_charset', 'UTF-8');
+mb_internal_encoding('UTF-8');
+iconv_set_encoding('internal_encoding', 'UTF-8');
+iconv_set_encoding('output_encoding', 'UTF-8');
+
 $pattern='/^\S/';
-$array=[];
 
 $stderr = fopen('php://stderr', 'w');
 fwrite($stderr,"Введите, пожалуйста, Имя".PHP_EOL);
@@ -17,17 +18,18 @@ fscanf(STDIN, "%s\n", $patronymicUser);
 
 
 $temporaryVariable = preg_match_all($pattern, $nameUser ,$outNameUser);
-$temporaryVariable = preg_match_all($pattern, $lastnameUser ,$outLastNameUser);
-$temporaryVariable = preg_match_all($pattern, $patronymicUser ,$outPatronymicUser);
+$temporaryVariable2 = preg_match_all($pattern, $lastnameUser ,$outLastNameUser);
+$temporaryVariable3 = preg_match_all($pattern, $patronymicUser ,$outPatronymicUser);
 
 //вывод фио
-$fio = strtoupper($outNameUser[0][0]).strtoupper($outLastNameUser[0][0]).strtoupper($outPatronymicUser[0][0]);
+$fio = mb_strtoupper ($outLastNameUser[0][0].($outNameUser[0][0]).$outPatronymicUser[0][0]);
 echo $fio.PHP_EOL;
 
 //вывод Фамилия Имя Отчество
-$fullName = preg_replace($pattern, strtoupper($outNameUser[0][0]), $nameUser).' '.preg_replace($pattern, strtoupper($outLastNameUser[0][0]), $lastnameUser).' '.preg_replace($pattern, strtoupper($outPatronymicUser[0][0]), $patronymicUser);
+$fullName = preg_replace($pattern, mb_strtoupper ($outLastNameUser[0][0]), $lastnameUser).' '.preg_replace($pattern, mb_strtoupper ($outNameUser[0][0]), $nameUser).' '.preg_replace($pattern, mb_strtoupper ($outPatronymicUser[0][0]), $patronymicUser);
 echo $fullName.PHP_EOL;
 
 //вывод Фамилия И.О.
-$surnameAndInitials = preg_replace($pattern, strtoupper($outNameUser[0][0]), $nameUser).' '.strtoupper($outLastNameUser[0][0]).'.'.strtoupper($outPatronymicUser[0][0]);
+$surnameAndInitials = preg_replace($pattern,mb_strtoupper ($outLastNameUser[0][0]) , $lastnameUser).' '.mb_strtoupper ($outNameUser[0][0]).'.'.mb_strtoupper ($outPatronymicUser[0][0]).'.';
+
 echo $surnameAndInitials.PHP_EOL;
